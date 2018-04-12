c7ceacd7da72f4cba7a708d1f6837dda7e0fecb7
CatherineIsNotHere <37690797+CatherineIsNotHere@users.noreply.github.com>
2018-04-12 17:26:33 +0800

    20180412
    课后练习打图形

--- lesson15-5/lesson15-5/课上练习.cpp
+++ lesson15-5/lesson15-5/课上练习.cpp
@@ -1,15 +1,15 @@
 #include "stdio.h"
 
 void test1(int t){
-	for (int i=1; i <= t; i++)
+	for (int i = 1; i <= t; i++)
 	{
-		for (int j= 1; j <= i; j++)
+		for (int j = 1; j <= i; j++)
 		{
 			printf("*");
 		}
 		printf("\n");
 	}
-	
+
 }
 
 
@@ -21,28 +21,72 @@ void test2(int t){//t=7
 	// *****  //7/3=3-2 1空格 5星
 	//	***	//7/2=3-1 2空格 3星
 	//   *    //7/2=3 3空格 1星
-	int num = t / 2;
-	for (num + 1; num >= 0; num--)
+	int up = t / 2 + 1;//上半部分的行个数
+	int k = t / 2;//空格个数
+	for (up; up > 0; up--)
 	{
-		int j = t / 2;
-		for (j; j > t / 2 - num; j--)
+		int j = k;
+		for (j; j > 0; j--)
 		{
 			printf(" ");
 		}
+		int i = t - 2 * k;
+		for (i; i > 0; i--)
+		{
+			printf("*");
+		}
+		printf("\n");
+		k--;
 	}
-	int num2 = t / 2;
-	for (num2; num2 >= 0; num2--)
+
+	int down = t % 2 == 0 ? t / 2 + 1 : t / 2;//下半部分的行个数
+	k = t % 2 == 0 ? 0 : 1;//空格个数
+	for (down; down > 0; down--)
 	{
-		for (int j = 1; j <= t / 2 - num2; j++)
+		int j = k;
+		for (j; j > 0; j--)
 		{
 			printf(" ");
 		}
-		printf("*\n");
+		int i = t - 2 * k;
+		for (i; i > 0; i--)
+		{
+			printf("*");
+		}
+		printf("\n");
+		k++;
 	}
 }
 
 void test3(int t){
-
+	 //     1       行数6 空格 5
+	 //    121
+	 //   12321
+	 //  1234321
+	 // 123454321
+	 //12345654321
+	int k = t - 1;
+	int h = t;
+	for (h; h > 0; h--)
+	{
+		int i = k;
+		for (i; i > 0; i--)
+		{
+			printf(" ");
+		}
+		int num = 2*t-1-2*k;//总宽度减2倍空格数也就是每一行有多少个数
+		int mnum = num / 2 + 1;//得出中间最大的数
+		for (int j=1; j < mnum; j++)
+		{
+			printf("%d",j);
+		}
+		for (mnum; mnum > 0 ; mnum--)
+		{
+			printf("%d", mnum);
+		}
+		printf("\n");
+		k--;
+	}
 }
 void main(){
 	int t1 = 0;
