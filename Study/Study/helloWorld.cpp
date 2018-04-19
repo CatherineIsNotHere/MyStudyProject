ad160d562a44c01bdfc36468b22cdbcf5ee4c757
CatherineIsNotHere <37690797+CatherineIsNotHere@users.noreply.github.com>
2018-04-19 20:23:55 +0800

    20180419
    整改删除

--- Study/Study/helloWorld.cpp
+++
@@ -1,6 +0,0 @@
-#include "stdio.h"
-// helloWorld打印程序 所有语言的第一课 
-int main(){
-	printf("hello c++");
-	getchar();
-}
--- Study/Study/lesson2.cpp
+++
@@ -1,6 +0,0 @@
-#include "stdio.h"
-int bbb(){
-	printf("123123123");
-	getchar();
-	return 1;
-}
