d9376510639db669d38f8a3cc64b2eb68f759f93
CatherineIsNotHere <37690797+CatherineIsNotHere@users.noreply.github.com>
2018-04-23 07:46:30 +0800

    20180423
    家中代码

--- huiZhongLesson/lesson21-2/lesson21-2/文件打包.cpp
+++ huiZhongLesson/lesson21-2/lesson21-2/文件打包.cpp
@@ -1,5 +1,16 @@
 #include <stdio.h>
+#include <string.h>  
+#include <io.h>  
 #include "文件操作.h"
+
+
+struct AllFile
+{
+	char filePath[256];
+	char fileName[256];
+	long fileSize;
+	long position;
+};
 
 
 void main(){
@@ -10,10 +21,19 @@ void main(){
 	int size = sizeof(fileName)/sizeof(fileName[0]);
 	FILE* file = new FILE;
 	FILE* targetFile = openFileForWriteBinery(targetFileName);
-	for (int i = 0; i < 1; i++)
+	AllFile* fileDetails = new AllFile[size];
+	for (int i = 0; i < size; i++)
 	{
-		file = openFileForReadBinery(fileName[i]);
+		strcpy(fileDetails[i].filePath, fileName[i]);
+		file = openFileForReadBinery(fileDetails[i].filePath);//读取每个文件
 		if (file != nullptr){
+			int no=_fileno(file);
+			fileDetails[i].fileSize = _filelength(no);
+			if (i==0)
+			{
+				//fileDetails[i].position=
+			}
+		
 
 		}
 		else
