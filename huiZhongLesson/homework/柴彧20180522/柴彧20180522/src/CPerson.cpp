5dc93e36ffe07978bb22f4df7a59a53b2406fff0
CatherineIsNotHere <37690797+CatherineIsNotHere@users.noreply.github.com>
2018-05-22 12:15:08 +0800

    20180522
    课堂作业

---
+++ huiZhongLesson/homework/柴彧20180522/柴彧20180522/src/CPerson.cpp
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+#include <string>
+using namespace std;
+
+class CPerson{
+public:
+	int age=0;
+	float height=0;
+	float weight=0;
+	static char* born;
+	static void cry(){
+		cout << "I am crying!" << endl;
+	}
+	static void eat(){
+		cout << "I am eating!" << endl;
+	}
+	static void sleep(){
+		cout << "I am sleeping!" << endl;
+	}
+	void work(){
+		cout << "I am working" << endl;
+	}
+
+};
+char* CPerson::born = { "Îäºº" };
+void main(){
+	cout << CPerson::born << endl;
+	CPerson::cry();
+	CPerson::eat();
+	CPerson::sleep();
+
+	system("pause");
+}
