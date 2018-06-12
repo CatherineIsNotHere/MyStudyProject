e19da7474ca3d235ce3aa19329ecfce660146ea1
CatherineIsNotHere <37690797+CatherineIsNotHere@users.noreply.github.com>
2018-06-12 12:33:30 +0800

    20180612
    线条

--- huiZhongLesson/lesson45-1/lesson45-1/WinApp.hpp
+++ huiZhongLesson/lesson45-1/lesson45-1/WinApp.hpp
@@ -1,6 +1,8 @@
 #pragma once
 #include <windows.h>
 #include <tchar.h>
+#include<math.h> 
+
 class CWinApp
 {
 public:
@@ -9,7 +11,7 @@ public:
 	{
 		WNDCLASSEX wc;
 		wc.cbSize = sizeof(WNDCLASSEX);
-		wc.style = CS_HREDRAW|CS_VREDRAW|CS_OWNDC;
+		wc.style = CS_HREDRAW | CS_VREDRAW | CS_OWNDC;
 		wc.lpfnWndProc = wndProc;
 		wc.cbClsExtra = 0;
 		wc.cbWndExtra = 0;
@@ -23,7 +25,7 @@ public:
 		RegisterClassEx(&wc);
 	};
 	virtual ~CWinApp(){
-		UnregisterClass(TEXT("winApp"),m_hInst);
+		UnregisterClass(TEXT("winApp"), m_hInst);
 	}
 	HINSTANCE getHInst(){
 		return m_hInst;
@@ -38,35 +40,37 @@ public:
 		return m_nHeight;
 	}
 	void setHInst(HINSTANCE hInstance){
-		this->m_hInst=hInstance;
+		this->m_hInst = hInstance;
 	}
 	void setHWnd(HWND hwnd){
-		this-> m_hWnd=hwnd;
+		this->m_hWnd = hwnd;
 	}
 	void setNWidth(int width){
-		this-> m_nWidth=width;
+		this->m_nWidth = width;
 	}
 	void setNHeight(int height){
-		this-> m_nHeight=height;
+		this->m_nHeight = height;
 	}
 	static LRESULT CALLBACK wndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam){
 		CWinApp* pthis = (CWinApp *)GetWindowLong(hwnd, GWLP_USERDATA);
 		if (pthis){
 			return pthis->onEvent(hwnd, msg, wParam, lParam);
 		}
-		if (WM_CREATE==msg){
+		if (WM_CREATE == msg){
 			CREATESTRUCT * pCreate = (CREATESTRUCT*)lParam;
 			SetWindowLong(hwnd, GWL_USERDATA, (DWORD_PTR)pCreate->lpCreateParams);
 		}
 		return  DefWindowProc(hwnd, msg, wParam, lParam);
 	}
-	 virtual LRESULT onEvent(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam){
-		 HDC hdc;
-		 PAINTSTRUCT ps;
-		 TCHAR szText[64] = TEXT("这是测试汉字this is test!");
-		 RECT rc;//矩形
-		 HFONT hfont;
-		 HFONT hOldFont;
+	virtual LRESULT onEvent(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam){
+		HDC hdc;
+		PAINTSTRUCT ps;
+		TCHAR szText[64] = TEXT("这是测试汉字this is test!");
+		RECT rc;//矩形
+		HFONT hfont;
+		HFONT hOldFont;
+		double y = 0;
+		double r = 100;
 		switch (msg)
 		{
 		case WM_CLOSE:
@@ -76,14 +80,34 @@ public:
 			break;
 		case WM_MOUSEMOVE:
 			break;
+		case WM_KEYUP:
+			switch (wParam)
+			{
+			case VK_SPACE:
+				break;
+			case VK_ESCAPE:
+				PostQuitMessage(0);
+				break;
+			default:
+				break;
+			}
+			break;
+		case WM_KEYDOWN:
+			break;
+		case WM_SETFOCUS:
+			SetWindowText(hwnd,TEXT("winApp"));
+			break;
+		case WM_KILLFOCUS:
+			SetWindowText(hwnd, TEXT("winApp (未响应)"));
+			break;
 		case WM_PAINT:
-			hdc = BeginPaint(hwnd,&ps);//开始绘图
+			hdc = BeginPaint(hwnd, &ps);//开始绘图
 			SetBkMode(hdc, TRANSPARENT);//去掉背景框
-			SetTextColor(hdc,RGB(255,0,255));//文字颜色
+			SetTextColor(hdc, RGB(255, 0, 255));//文字颜色
 			//TextOut(hdc, 100, 200, szText, _tcslen(szText));//输出文字，可以精准定位xy,不能换行
-			GetClientRect(hwnd,&rc);//获取客户区尺寸
+			GetClientRect(hwnd, &rc);//获取客户区尺寸
 			//DrawText(hdc,szText,-1,&rc,DT_SINGLELINE|DT_CENTER|DT_VCENTER);
-			hfont=CreateFont(
+			hfont = CreateFont(
 				50,//字符高度
 				0,//字符宽度
 				0,//文字倾斜角度
@@ -99,23 +123,55 @@ public:
 				0,//字体系列和字体宽度设定
 				TEXT("楷体")//字样名
 				);
-			hOldFont = (HFONT)SelectObject(hdc,hfont);//选择新的字体，返回老的字体
-			SetTextColor(hdc,RGB(0X00,0Xff,0Xff));
-			TextOut(hdc,0,300,szText,_tcslen(szText));
+			hOldFont = (HFONT)SelectObject(hdc, hfont);//选择新的字体，返回老的字体
+			SetTextColor(hdc, RGB(0X00, 0Xff, 0Xff));
+			TextOut(hdc, 0, 300, szText, _tcslen(szText));
 			SelectObject(hdc, hOldFont);
 			DeleteObject(hfont);
-			EndPaint(hwnd,&ps);//释放内存
+
+			//画一条线
+			/*for (int m = 0; m < 100;m++)
+			{
+			SetPixel(hdc,m,100,RGB(0,255,0));
+			}*/
+			//画一个圆
+			for (double x = 100; x > 0; x--)
+			{
+				y = sqrt(r*r - x*x);
+				SetPixel(hdc, x + 200, y + 200, RGB(0, 255, 0));
+				SetPixel(hdc, y + 200, x + 200, RGB(0, 255, 0));
+			}
+			for (double x = 100; x > 0; x--)
+			{
+				y = sqrt(r*r - x*x);
+				SetPixel(hdc, 200 - x, 200 + y, RGB(0, 255, 0));
+				SetPixel(hdc, 200 + y, 200 - x, RGB(0, 255, 0));
+			}
+			for (double x = 100; x > 0; x--)
+			{
+				y = sqrt(r*r - x*x);
+				SetPixel(hdc, 200 - x, 200 - y, RGB(0, 255, 0));
+				SetPixel(hdc, 200 - y, 200 - x, RGB(0, 255, 0));
+			}
+			for (double x = 100; x > 0; x--)
+			{
+				y = sqrt(r*r - x*x);
+				SetPixel(hdc, 200 + x, 200 - y, RGB(0, 255, 0));
+				SetPixel(hdc, 200 - y, 200 + x, RGB(0, 255, 0));
+			}
+
+			EndPaint(hwnd, &ps);//释放内存
 			break;
 		default:
 			break;
 		}
 		return DefWindowProc(hwnd, msg, wParam, lParam);
 	}
-	int Run(int w,int h){
+	int Run(int w, int h){
 		m_nWidth = w;
 		m_nHeight = h;
 		m_hWnd = CreateWindowEx(
-			NULL, TEXT("winApp"), TEXT("winApp"), 
+			NULL, TEXT("winApp"), TEXT("winApp"),
 			WS_OVERLAPPEDWINDOW,
 			0,
 			0,
@@ -125,15 +181,15 @@ public:
 			m_hInst,
 			this
 			);
-		if (m_hWnd==0){
+		if (m_hWnd == 0){
 			return -1;
 		}
 		ShowWindow(m_hWnd, SW_SHOW);
 		UpdateWindow(m_hWnd);
-		MSG msg = {0};
+		MSG msg = { 0 };
 		while (msg.message != WM_QUIT)
 		{
-			if (PeekMessage(&msg,NULL,0,0,PM_REMOVE)){
+			if (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE)){
 				TranslateMessage(&msg);
 				DispatchMessage(&msg);
 			}
