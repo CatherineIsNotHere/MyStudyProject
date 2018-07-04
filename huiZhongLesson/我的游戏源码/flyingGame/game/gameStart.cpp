a28e7611d81ffdeefbedddeb231bfb8250a49aa2
CatherineIsNotHere <37690797+CatherineIsNotHere@users.noreply.github.com>
2018-07-04 17:05:56 +0800

    20180704
    飞机大战

---
+++ huiZhongLesson/我的游戏源码/flyingGame/game/gameStart.cpp
@@ -0,0 +1,48 @@
+#include"hgefont.h"
+#include "hge.h"
+#include "hgeanim.h"
+#include "hgeSprite.h" 
+HGE *hge = 0;
+hgeAnimation* g_animat;
+HTEXTURE g_tex;
+bool RenderFunc(){//绘制的回调，程序开始后会被不停的回调
+	hge->Gfx_BeginScene();//开始绘图
+	hge->Gfx_Clear(0x00000000);
+	g_animat->Render(100, 200);
+	hge->Gfx_EndScene();
+	return false;//正常应返回false
+}
+bool FrameFunc(){//输入的回调，程序开始后会被不停的回调
+	float dt = hge->Timer_GetDelta();
+	g_animat->Update(dt); // 更新
+	if (hge->Input_GetKeyState(HGEK_ESCAPE))
+	{
+		return true;
+	}
+	return false;//正常应返回false
+}
+
+int WINAPI WinMain(HINSTANCE hinstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nShowCmd){
+	hge = hgeCreate(HGE_VERSION);//创建
+	hge->System_SetState(HGE_SCREENWIDTH, 800);
+	hge->System_SetState(HGE_SCREENHEIGHT, 600);
+	hge->System_SetState(HGE_WINDOWED, true);
+	hge->System_SetState(HGE_TITLE, "hello hge");
+	hge->System_SetState(HGE_USESOUND, false);
+	hge->System_SetState(HGE_RENDERFUNC, RenderFunc);
+	hge->System_SetState(HGE_FRAMEFUNC, FrameFunc);
+	hge->System_SetState(HGE_SHOWSPLASH, false);
+	if (hge->System_Initiate()){//初始化
+		g_tex = hge->Texture_Load("飞机1号.png");
+		g_animat = new hgeAnimation(g_tex, 20, 20, 0, 0, 100, 90);
+		g_animat->SetBlendMode(BLEND_COLORMUL | BLEND_ALPHAADD | BLEND_NOZWRITE);
+		g_animat->SetSpeed(10);// 速度
+		g_animat->Play();
+		hge->System_Start();//开始运行
+	}
+	delete g_animat;
+	hge->System_Shutdown();//结束程序
+	hge->Release();//释放
+
+	return 0;
+}
