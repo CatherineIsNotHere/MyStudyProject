18209e532dc8c0ce4d1dafd2adf19c63f53643c9
CatherineIsNotHere <37690797+CatherineIsNotHere@users.noreply.github.com>
2018-08-02 12:28:06 +0800

    20180802
    并行

--- huiZhongLesson/Unity/lesson1_ball/Assets/script/playerSphere.cs
+++ huiZhongLesson/Unity/lesson1_ball/Assets/script/playerSphere.cs
@@ -5,7 +5,7 @@ public class playerSphere : MonoBehaviour {
     float jump_speed = 5.0f;
 	// Use this for initialization
 	void Start () {
-        GetComponent<Rigidbody>().velocity = new Vector3(0, 0, 15);
+        GetComponent<Rigidbody>().velocity = new Vector3(0, -5, 0);
 	}
 	
 	// Update is called once per frame
---
+++ huiZhongLesson/Unity/lesson1_ball/Assets/script/test.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+//携程和repeating
+public class test : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        //InvokeRepeating("Printf",0.1f,3.0f);//每三秒打印一次名为Printf函数
+        StartCoroutine(Test1());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+    IEnumerator Test1() {
+        while(true){
+            Debug.Log("aaa");
+            yield return new WaitForSeconds(2.0f);
+            Debug.Log("ccc");
+            yield return new WaitForSeconds(2.0f);
+        }
+    }
+}
